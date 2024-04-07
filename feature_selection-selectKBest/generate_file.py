1f0222df85e839c2b1e9fbbcff2eabf520f7f69a
Yuke Wu <143554902+yukewF2023@users.noreply.github.com>
2024-04-07 13:24:34 -0700

    Minor update: generate_file.py

--- feature_selection-selectKBest/generate_file.py
+++ feature_selection-selectKBest/generate_file.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('data/processedData.csv')
 
-columns_to_keep = ['prior_review_num', 'first_response_time', 'inte_open', 'account_creation_days', 'contrib_perc_commit', 'team_size', 'open_issue_num', 'project_age', 'open_pr_num', 'pr_succ_rate', 'stars', 'integrator_availability', 'description_length', 'lifetime_minutes', 'ci_latency', 'ci_failed_perc', 'commits_on_files_touched', 'num_comments', 'first_pr', 'merged_or_not']
+columns_to_keep = ['first_response_time', 'account_creation_days', 'contrib_perc_commit', 'team_size', 'open_issue_num', 'project_age', 'open_pr_num', 'pr_succ_rate', 'stars', 'description_length', 'lifetime_minutes', 'ci_latency', 'ci_failed_perc', 'commits_on_files_touched', 'num_comments', 'merged_or_not']
 
 df = df[columns_to_keep]
 
